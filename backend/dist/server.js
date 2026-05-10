ddd2af9d12fb507343129db783dd63fb04fde73e
Wesley Ricardo <wesleyraiol.js@gmail.com>
2026-05-09 21:35:56 -0300

    chore: clean repository structure

--- backend/dist/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const PORT = Number(process.env.PORT) || 3333;
-app_1.app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
